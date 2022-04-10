7428bb570da7e692757c2feba93462d7b11f4cb2
esradinc <esradnc1907@icloud.com>
2022-04-10 16:29:23 +0300

    .

---
+++ yazlab2/konsol/DataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace konsol
+{
+    class DataModel
+    {
+
+        public string Id { get; set; }
+        public string Time { get; set; }
+        public string Lang { get; set; }
+        public string Lat { get; set; }
+    }
+}
---
+++ yazlab2/konsol/Program.cs
@@ -0,0 +1,40 @@
+﻿using Google.Cloud.Firestore;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace konsol
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            string dateInput = "2.10.2018 17:06:00";
+            var parsedDate = DateTime.Parse(dateInput);
+            Console.WriteLine(parsedDate);
+        }
+        public static DateTime UnixTimestampToDateTime(double unixTime)
+        {
+            DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            long unixTimeStampInTicks = (long)(unixTime * TimeSpan.TicksPerSecond);
+            return new DateTime(unixStart.Ticks + unixTimeStampInTicks, System.DateTimeKind.Utc);
+        }
+        public static double DateTimeToUnixTimestamp(DateTime dateTime)
+        {
+            DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            long unixTimeStampInTicks = (dateTime.ToUniversalTime() - unixStart).Ticks;
+            return (double)unixTimeStampInTicks / TimeSpan.TicksPerSecond;
+        }
+
+    }
+
+}
+
+   
+
+
